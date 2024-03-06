73aae91807f979f742827e9e66cc7fb708d22b14
cjahn1305 <152269511+cjahn1305@users.noreply.github.com>
2024-03-06 15:07:06 +0100

    Update AgeFunctions.test.js

--- AgeFunctions.test.js
+++ AgeFunctions.test.js
@@ -4,4 +4,36 @@ describe("Tests for isAdult Function", () => {
   test("Test for age being 18", () => {
     expect(isAdult(18)).toBe(true);
   });
+
+  test("Test for age being less than 18", () => {
+    expect(isAdult(17)).toBe(false);
+  });
+
+  test("Test for age being 0", () => {
+    expect(isAdult(0)).toBe(false);
+  });
+
+  test("Test for age being negative", () => {
+    expect(() => {
+      isAdult(-1);
+    }).toThrow(TypeError);
+  });
+
+  test("Test for age being a string", () => {
+    expect(() => {
+      isAdult("18");
+    }).toThrow(TypeError);
+  });
+
+  test("Test for age being undefined", () => {
+    expect(() => {
+      isAdult();
+    }).toThrow(TypeError);
+  });
+
+  test("Test for age being null", () => {
+    expect(() => {
+      isAdult(null);
+    }).toThrow(TypeError);
+  });
 });
